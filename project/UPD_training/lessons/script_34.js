54335de93a9557a709a9287884cfd51e50cc2a0f
Berkut_Dima <dmitriyb@wizardsdev.com>
2021-06-08 15:20:52 +0300

    lsn_34 events on mobile

---
+++ project/UPD_training/lessons/script_34.js
@@ -0,0 +1,34 @@
+//touchstart
+//touchmove
+//touchend
+// touchenter
+// touchleave
+// touchcancel
+
+window.addEventListener('DOMContentLoaded', () => {
+	const box = document.querySelector('.box');
+
+	box.addEventListener('touchstart', (e) => {
+		e.preventDefault();
+
+		console.log('start');
+		console.log(e.changedTouches);
+	})
+
+	// box.addEventListener('touchmove', (e) => {
+	// 	e.preventDefault();
+
+	// 	console.log('move');
+	// })
+
+	// box.addEventListener('touchend', (e) => {
+	// 	e.preventDefault();
+
+	// 	console.log('touchend');
+	// })
+
+})
+
+// touches
+// targetTouches
+// changedTouches
