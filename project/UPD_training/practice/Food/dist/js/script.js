1269de3199a2353afdbbbc17fd7cd388ffc545ea
Berkut_Dima <dmitriyb@wizardsdev.com>
2021-09-21 00:33:08 +0300

    lsn 53 sending formData to server

--- project/UPD_training/practice/Food/dist/js/script.js
+++ project/UPD_training/practice/Food/dist/js/script.js
@@ -104,7 +104,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		modal.classList.add('show');
 		modal.classList.remove('hide');
 		document.body.style.overflow = 'hidden';
-		clearInterval(modalTimerId);
+		// clearInterval(modalTimerId);
 	}
 
 	modalTrigger.forEach(btn => {
@@ -220,4 +220,60 @@ window.addEventListener('DOMContentLoaded', () => {
 			'.menu .container',
 			'menu__item'
 		).render()
+		
+		// Forms ===================================================
+		const forms = document.querySelectorAll('form');
+
+		const message = {
+			loading: 'Загрузка',
+			success: 'Спасибо с Вами скоро свяжутся',
+			failure: 'ЧТо-то пошло не так...'
+		};
+
+		forms.forEach(item => {
+			postData(item)
+		})
+
+		function postData(form) {
+			form.addEventListener('submit', (e) => {
+				e.preventDefault();
+
+				const statusMessege = document.createElement('div');
+				statusMessege.classList.add('status');
+				statusMessege.textContent = message.loading;
+				form.append(statusMessege)
+
+				const request = new XMLHttpRequest();
+				request.open('POST', 'server.php');
+
+				// В связке XMLHttpRequest и FormData заголовок не нужно устанавливать
+				// request.setRequestHeader('Content-type', 'mulipart/form-data');
+
+				request.setRequestHeader('Content-type', 'application/json');
+				const formData = new FormData(form);
+
+				const object = {};
+
+				formData.forEach(function(value, key) {
+					object[key] = value;
+				});
+
+				const json = JSON.stringify(object)
+
+				request.send(json);
+
+				request.addEventListener('load', () => {
+					if (request.status === 200) {
+						console.log(request.response);
+						statusMessege.textContent = message.success;
+						form.reset();
+						setTimeout(() => {
+							statusMessege.remove();
+						}, 2000)
+					} else {
+						statusMessege.textContent = message.failure;
+					}
+				})
+			})
+		}
 });
